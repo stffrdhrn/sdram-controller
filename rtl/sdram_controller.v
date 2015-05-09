33d02593ce4ef6389a9db4c38335c74ceda2e2c4
Stafford <shorne@gmail.com>
2015-05-09 22:31:01 +0900

    add some commands get module compiling

--- rtl/sdram_controller.v
+++ rtl/sdram_controller.v
@@ -19,57 +19,100 @@
  *  how long to wait for init, write operations and read operations
  */
 
+
+ 
+module sdram_controller (
+    /* HOST INTERFACE */
+    haddr,
+    data_input,
+    data_output,
+    busy,
+    rd_enable,
+    wr_enable,
+    rst_n,
+    clk,
+
+    /* SDRAM SIDE */
+    addr,   // 13 
+    bank_addr,  // 4 banks
+    data,
+    clock_enable,
+    cs_n,
+    ras_n,
+    cas_n,
+    we_n,
+    data_mask_low, // DQML
+    data_mask_high // DQMH
+);
+
+/* Internal Parameters */
 parameter ROW_WIDTH = 13;
 parameter COL_WIDTH = 9;
 parameter BANK_WIDTH = 2;
 
-parameter ADDR_WIDTH = ROW_WIDTH + COL_WIDTH + BANK_WIDTH;
+parameter SDRADDR_WIDTH = ROW_WIDTH > COL_WIDTH ? ROW_WIDTH : COL_WIDTH;
+parameter HADDR_WIDTH = ROW_WIDTH + COL_WIDTH + BANK_WIDTH;
  
 parameter CLK_FREQUENCY = 133; // Mhz
 parameter REFRESH_COUNT = 8192;
 parameter REFRESH_TIME =  32;  // ms
 
+// clk / refresh =  clk / sec 
+//                , sec / refbatch 
+//                , ref / refbatch
+parameter CYCLES_BETWEEN_REFRESH = ( ( CLK_FREQUENCY * 1000000 * REFRESH_TIME ) / 1000 ) / REFRESH_COUNT;
+
 // STATES - HIGH LEVEL
 parameter IDLE = 2'b00;
 parameter INIT = 2'b01;
 parameter REF =  2'b10;
-
-// clk / refresh =  clk / sec 
-//                , sec / refbatch 
-//                , ref / refbatch
-
-parameter CYCLES_BETWEEN_REFRESH = ( ( CLK_FREQUENCY * 1000000 * REFRESH_TIME ) / 1000 ) / REFRESH_COUNT;
  
-module sdram_controller (
-    /* HOST INTERFACE */
-    input  [ADDR_WIDTH-1:0] haddr,
-    input  [15:0] data_input,
-    output [15:0] data_output,
-    output busy,
-    input  rd_enable,
-    input  wr_enable,
-    input  rst_n,
-    input  clk,
+// Commands             CCRCWBBA
+//                      ESSSE100
+parameter CMD_PALL = 8'b10010001;
+parameter CMD_REF  = 8'b10001000;
+parameter CMD_NOP  = 8'b10111000;
+parameter CMD_MRS  = 8'b10000000;
 
-    /* SDRAM SIDE */
-    output [ROW_WIDTH-1:0]   row_addr,   // 13 
-    output [COL_WIDTH-1:0]   col_addr,   // +9 = 22-bit address = 4mbit
-    output [BANK_WIDTH-1:0]  bank_addr,  // 4 banks
-    inout  [15:0]            data,
-    output        clock_enable,
-    output        cs_n,
-    output        ras_n,
-    output        cas_n,
-    output        we_n,
-    output        data_mask_low, // DQML
-    output        data_mask_high // DQMH
-);
+/* Interface Definition */
+/* HOST INTERFACE */
+input  [HADDR_WIDTH-1:0]   haddr;
+input  [15:0]              data_input;
+output [15:0]              data_output;
+output                     busy;
+input                      rd_enable;
+input                      wr_enable;
+input                      rst_n;
+input                      clk;
 
-wire row_addr;
-wire column_addr;
-reg command;
+/* SDRAM SIDE */
+output [SDRADDR_WIDTH-1:0] addr;
+output [BANK_WIDTH-1:0]    bank_addr;
+inout  [15:0]              data;
+output                     clock_enable;
+output                     cs_n;
+output                     ras_n;
+output                     cas_n;
+output                     we_n;
+output                     data_mask_low;
+output                     data_mask_high;
 
-assign row_addr = addr;
-assign column_addr = addr[8:0];
+/* Internal Wiring */
+reg [7:0] command;
+
+assign clock_enable = command[7];
+assign cs_n         = command[6];
+assign ras_n        = command[5];
+assign cas_n        = command[4];
+assign we_n         = command[3];
+assign bank_addr[1] = command[2];
+assign bank_addr[0] = command[1];
+assign addr[10]     = command[0];
+
+always @ (posedge clk)
+  if (~rst_n)
+    command <= CMD_NOP;
+  else 
+    command <= command;
 
 endmodule
