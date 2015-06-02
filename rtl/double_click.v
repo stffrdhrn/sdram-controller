3021eeb0d8e85c8a14cbef8e01965c9fe3994856
Stafford <shorne@gmail.com>
2015-06-02 22:01:54 +0900

    dont need async reset because we are at the same clock now

--- rtl/double_click.v
+++ rtl/double_click.v
@@ -21,12 +21,10 @@ reg btn_now, btn_last, collect;
 
 reg [2:0] click_cnt;
 reg [WAIT_WIDTH-1:0] dbl_click_cnt;
-wire rst;
 
 // if we are done counting and we have 1 click its single, else double
 assign single = (!dbl_click_cnt & (click_cnt == 3'b001)) ? 1'b1 : 1'b0;
 assign double = (!dbl_click_cnt & (click_cnt != 3'b001)) ? 1'b1 : 1'b0;
-assign rst = ~rst_n;
 
 // detect button down vs button up
 wire btn_down = btn_now & ~btn_last;
@@ -38,8 +36,8 @@ always @ (negedge clk)
   { btn_last, btn_now } <= { btn_now, button };
 
 // start down counter and count clicks
-always @ (posedge clk or posedge rst) 
- if (rst)
+always @ (posedge clk) 
+ if (~rst_n)
   begin
   click_cnt <= 3'd0;
   dbl_click_cnt <= {WAIT_WIDTH{1'b1}};
